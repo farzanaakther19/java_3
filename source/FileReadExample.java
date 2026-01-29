4c458dd9f25fbe26d66ce2379394d0f8fcc013e8
farzanaakther19 <farzanaakthar33@gmail.com>
2026-01-29 10:51:34 +0530

    Create FileReadExample.java for file reading demo
    Added a basic file reading example that demonstrates handling a FileNotFoundException.

---
+++ source/FileReadExample.java
@@ -0,0 +1,8 @@
+// FileReadExample.java
+import java.io.*;
+public class FileReadExample {
+public static void main(String[] args) throws FileNotFoundException {
+FileInputStream fis = new FileInputStream("no-such-file.txt");
+// Compiler requires handling or declaring FileNotFoundException
+}
+}
